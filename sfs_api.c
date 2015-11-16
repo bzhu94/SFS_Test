bb9c1b7fc73c5ef134ccc457153d7e83db863eff
Brian Zhu <appleuser@Apples-MacBook-Air.local>
2015-11-16 09:59:53 -0800

    no message

---
+++ sfs_api.c
@@ -0,0 +1,60 @@
+#include "sfs_api.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int seen = 0;
+
+void mksfs(int fresh) {
+	//Implement mksfs here	
+	return;
+}
+
+int sfs_getnextfilename(char *fname) {
+
+	//Implement sfs_getnextfilename here	
+	return 0;
+}
+
+
+int sfs_getfilesize(const char* path) {
+
+	//Implement sfs_getfilesize here	
+	return 0;
+}
+
+int sfs_fopen(char *name) {
+
+	//Implement sfs_fopen here	
+	return 0;
+}
+
+int sfs_fclose(int fileID){
+
+	//Implement sfs_fclose here	
+	return 0;
+}
+
+int sfs_fread(int fileID, char *buf, int length){
+
+	//Implement sfs_fread here	
+	return 0;
+}
+
+int sfs_fwrite(int fileID, const char *buf, int length){
+
+	//Implement sfs_fwrite here	
+	return 0;
+}
+
+int sfs_fseek(int fileID, int loc){
+
+	//Implement sfs_fseek here	
+	return 0;
+}
+
+int sfs_remove(char *file) {
+
+	//Implement sfs_remove here	
+	return 0;
+}
